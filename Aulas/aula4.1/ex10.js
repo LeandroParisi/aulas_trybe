18a70fba4de2c0cee1bb2d5367cc3bcae5f2d762
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-13 18:37:15 -0300

    Ex.10

---
+++ Aulas/aula4.1/ex10.js
@@ -0,0 +1,7 @@
+let custo = 50;
+let imposto = (custo*0.2)
+let venda = 150;
+
+let lucro = (1000*venda) - (1000*(custo+imposto))
+
+console.log(lucro)
---
+++ Aulas/aula4.1/ex9.js
@@ -0,0 +1,10 @@
+let a = 2;
+let b = 2;
+let c = 4;
+
+if ( a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0 ){
+    console.log("True")
+}
+else {
+    console.log("False")
+}
