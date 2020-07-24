ceca825c111c4e160dcc98d14ebae8f98c38ea8d
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-24 19:23:26 -0300

    Aula 5.4

--- Aulas/Aula5.4/ex1script.js
+++ Aulas/Aula5.4/ex1script.js
@@ -1,19 +1,43 @@
+// localStorage.setItem("textColor", "white");
+// localStorage.setItem("fontSize", "14px");
+// localStorage.setItem("lineSpacing", "1em");
+// localStorage.setItem("fontFamily", "sans-serif");
+
+
+// body.style.backgroundColor = localStorage.getItem("bgColor");
+// headingColor.style.color = localStorage.getItem("textColor");
+
+// for (let index = 0; index < paragraphs.length; index += 1){
+//     paragraphs[index].style.color = localStorage.getItem("textColor");
+// }
+// document.body.style.backgroundColor = localStorage.getItem("bgColor");
+
+
+
+
+
+
 let body = document.querySelector("body");
 let headingColor = document.querySelector("h1")
 let paragraphs = document.querySelectorAll("p");
 
 
-localStorage.setItem("bgColor", "black");
-localStorage.setItem("textColor", "white");
-localStorage.setItem("fontSize", "14px");
-localStorage.setItem("lineSpacing", "1em");
-localStorage.setItem("fontFamily", "sans-serif");
+document.body.style.backgroundColor = localStorage.getItem("bgColor");
 
+let blueBtn = document.querySelector(".blue");
 
-body.style.backgroundColor = localStorage.getItem("bgColor");
-headingColor.style.color = localStorage.getItem("textColor");
-
-for (let index = 0; index < paragraphs.length; index += 1){
-    paragraphs[index].style.color = localStorage.getItem("textColor");
+function setBackground(){
+    document.body.style.backgroundColor = blueBtn.className;
+    localStorage.setItem ("bgColor", blueBtn.className)
 }
 
+blueBtn.addEventListener("click", setBackground);
+
+let redBtn = document.querySelector(".red");
+
+function setBackground2(){
+    document.body.style.backgroundColor = redBtn.className;
+    localStorage.setItem ("bgColor", redBtn.className)
+}
+
+redBtn.addEventListener("click", setBackground2);
